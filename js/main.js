0a1e8ad9197da1dbc1a56176fe8f26d91e5661bf
haniiven <haniiven721@gmail.com>
2024-06-20 00:49:13 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+window.addEventListener("load", ()=>{
+    const grid = new Isotope("section", {
+        itemSelector:"article",
+        columnWidth:"article",
+        transitionDuration: "0.5s"
+    });
+});
+
+
